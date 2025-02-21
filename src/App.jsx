0fd12217b54b06e455be76cc763bdc9694a83026
allesonsales <allesonsales@gmail.com>
2025-02-21 12:40:53 -0300

    erro vitelogo

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,4 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 import Menu from './components/menu/menu'
 import Home from './pages/home/home'
